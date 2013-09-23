4af1f38293a3433dd093233bc2d3180a97b462a0
Louise Au Yeung <louise@eabx.org>
2013-09-23 09:12:19 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodTracker::Application.config.secret_key_base = '11585c360511f9a1e51d3d282a4f9add36032742dbee87abae9ca96eada05effb6f8a1b2139f9b45e7a6dbd4239a33277df031276510150ccf0d69668dd328f8'

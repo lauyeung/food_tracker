95455e9dcf26b84281886a024e052446548bf4dc
Louise Au Yeung <louise@eabx.org>
2013-09-23 11:35:15 -0400

    added feature test

---
+++ spec/features/enter_food_inventory_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'user enters food inventory', %Q{
+  As a food service employee
+  I want to receive inventory
+  So that it can be recorded that we have food items
+  } do
+
+# Acceptance Criteria:
+# * I must specify a title, description, and quantity of the food item
+# * If I do not specify the required information, I am prompted to fix errors and to resubmit
+# * If I specify the required information, my inventory entry is recorded.
+
+
+
+end
